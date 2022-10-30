50cd407aa82f979f0b98c40dd52bec8594cf1cf1
LuxHollow <98395537+LuxHollow@users.noreply.github.com>
2022-10-30 19:40:00 +0900

    javascript

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const paginazione = document.getElementById('paginazione');
+const labels = document.querySelectorAll('#paginazione > label');
+const pagine = document.getElementsById('pagine');
+const radioBtn = document.querySelectorAll('#pagine > input[type=radio]');
+let toccoInizioX = 0;
+let toccoFineX = 0;
 let currentPage = 1; 
 
 paginazione.addEventListener('click', e => {
@@ -9,9 +14,28 @@ paginazione.addEventListener('click', e => {
 })
 
 function setCheckedLabel(numberPageSelected) {
-    const labels = document.querySelectorAll('#paginazione > label');
     labels[currentPage-1].classList.remove('lab-sel');
     labels[numberPageSelected-1].classList.add('lab-sel');
     currentPage = numberPageSelected;
-  
+}
+
+pagine.addEventListener('touchstart', touchStart, false);
+pagine.addEventListener('touchend', touchEnd, false);
+
+function touchStart(e) {
+    toccoInizioX = e.changedTouches[0].screenX;
+}
+
+function touchEnd(e) {
+    toccoFineX = e.changedTouches[0].screenX;
+    toccoFineX < toccoInizioX ? moveTo('next') : moveTo ('prev');
+}
+
+function moveTo(direction) {
+    if(direction === 'next' && currentPage < labels.lenght) {
+        labels[currentPage-1].classList.remove('lab-sel');
+        labels[currentPage].classList.add('lab-sel');
+        radioBtn[currentPage].checked = true;
+        currentPage++;
+    }
 }
